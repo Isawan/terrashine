2e3115cde002413706f9f9ee81877bb9b0e48f9c
Isawan Millican <admin@isawan.net>
2023-04-16 21:19:36 +0100

    Correctly handle spans across await points

--- src/refresh.rs
+++ src/refresh.rs
@@ -1,11 +1,12 @@
 use std::{
     collections::{hash_map::Entry, HashMap},
+    mem,
     time::{Duration, Instant},
 };
 
 use sqlx::PgPool;
 use tokio::sync::{self, oneshot};
-use tracing::info_span;
+use tracing::{info_span, instrument, Instrument};
 
 use crate::{
     error::TerrashineError,
@@ -48,62 +49,65 @@ pub(crate) async fn refresher(
     let mut last_refresh = HashMap::new();
     while let Some(message) = rx.recv().await {
         let span = info_span!("refresh_request", provider = ?message.provider);
-        let _enter = span.enter();
-        tracing::debug!("Received refresh request");
-        let provider = message.provider;
-        let response_channel = message.response_channel;
-        let last_refreshed_entry = last_refresh.entry(provider);
-        match last_refreshed_entry {
-            Entry::Vacant(v) => {
-                tracing::info!(
-                    "Terraform provider not known to local instance, requesting upstream"
-                );
-                let key = v.key();
-                let result = refresh_versions(
-                    db,
-                    registry,
-                    key.hostname.as_str(),
-                    key.namespace.as_str(),
-                    key.provider_type.as_str(),
-                )
-                .await;
-                v.insert(Instant::now());
-                if let Some(sender) = response_channel {
-                    let result = sender.send(RefreshResponse::RefreshPerformed(result));
-                    if let Err(e) = result {
-                        tracing::error!(reason=?e, "Error responding to refresh request");
+        async {
+            tracing::debug!("Received refresh request");
+            let provider = message.provider;
+            let response_channel = message.response_channel;
+            let last_refreshed_entry = last_refresh.entry(provider);
+            match last_refreshed_entry {
+                Entry::Vacant(v) => {
+                    tracing::info!(
+                        "Terraform provider not known to local instance, requesting upstream"
+                    );
+                    let key = v.key();
+                    let result = refresh_versions(
+                        db,
+                        registry,
+                        key.hostname.as_str(),
+                        key.namespace.as_str(),
+                        key.provider_type.as_str(),
+                    )
+                    .await;
+                    v.insert(Instant::now());
+                    if let Some(sender) = response_channel {
+                        let result = sender.send(RefreshResponse::RefreshPerformed(result));
+                        if let Err(e) = result {
+                            tracing::error!(reason=?e, "Error responding to refresh request");
+                        }
                     }
                 }
-            }
-            Entry::Occupied(mut o) if Instant::now() - *o.get() > refresh_interval => {
-                tracing::info!("Provider is stale, updating provider");
-                let key = o.key();
-                let result = refresh_versions(
-                    db,
-                    registry,
-                    key.hostname.as_str(),
-                    key.namespace.as_str(),
-                    key.provider_type.as_str(),
-                )
-                .await;
-                o.insert(Instant::now());
-                if let Some(sender) = response_channel {
-                    let result = sender.send(RefreshResponse::RefreshPerformed(result));
-                    if let Err(e) = result {
-                        tracing::error!(reason=?e, "Error responding to refresh request");
+                Entry::Occupied(mut o) if Instant::now() - *o.get() > refresh_interval => {
+                    tracing::info!("Provider is stale, updating provider");
+                    let key = o.key();
+                    let result = refresh_versions(
+                        db,
+                        registry,
+                        key.hostname.as_str(),
+                        key.namespace.as_str(),
+                        key.provider_type.as_str(),
+                    )
+                    .await;
+                    o.insert(Instant::now());
+                    if let Some(sender) = response_channel {
+                        let result = sender.send(RefreshResponse::RefreshPerformed(result));
+                        if let Err(e) = result {
+                            tracing::error!(reason=?e, "Error responding to refresh request");
+                        }
                     }
                 }
-            }
-            // Do nothing if interval has not passed.
-            Entry::Occupied(o) => {
-                tracing::trace!("Provider is not stale, ignoring request to refresh");
-                if let Some(sender) = response_channel {
-                    let result = sender.send(RefreshResponse::ProviderVersionNotStale);
-                    if let Err(e) = result {
-                        tracing::error!(reason=?e, "Error responding to refresh request");
+                // Do nothing if interval has not passed.
+                Entry::Occupied(o) => {
+                    tracing::trace!("Provider is not stale, ignoring request to refresh");
+                    if let Some(sender) = response_channel {
+                        let result = sender.send(RefreshResponse::ProviderVersionNotStale);
+                        if let Err(e) = result {
+                            tracing::error!(reason=?e, "Error responding to refresh request");
+                        }
                     }
                 }
-            }
-        };
+            };
+        }
+        .instrument(span)
+        .await
     }
 }
