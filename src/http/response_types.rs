bd11b1fa8314bc21fa1ba802a11e9839b59cc0f1
Isawan Millican <admin@isawan.net>
2023-08-13 22:40:10 +0100

    chore: Remove unused code

--- src/http/response_types.rs
+++ src/http/response_types.rs
@@ -1,18 +1,5 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use url::Url;
-
-/// Versions response from terrashine mirror registry
-#[derive(Serialize)]
-pub(crate) struct MirrorVersions {
-    pub(crate) archives: HashMap<String, MirrorDownloadDetail>,
-}
-
-#[derive(Serialize)]
-pub(crate) struct MirrorDownloadDetail {
-    pub(crate) url: Url,
-    pub(crate) hashes: Option<Vec<String>>,
-}
 
 #[derive(Deserialize, Serialize, Debug)]
 pub(crate) struct MirrorVersion {
